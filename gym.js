32633700123501f9e9946294296e15e657d58cf1
Ansh <anshpathania@gmail.com>
2024-09-14 22:56:19 +0530

    zindex add in gs

--- gym.js
+++ gym.js
@@ -13,9 +13,10 @@ var y = document.getElementById("form_demo");
 function demo() {
     x.style.opacity = 0; // Hide inquiry form
     y.style.opacity = 1; // Show demo form
+    y.style.zIndex = 0;
 }
 function inquiry(){
     x.style.opacity = 1; // Hide inquiry form
     y.style.opacity = 0; // Show demo form
-
+    y.style.zIndex = -1;
 }
