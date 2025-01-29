cc66345cf4f22bec74bf86b599092d16703d08b4
Ansh <anshpathania@gmail.com>
2025-01-30 01:52:33 +0530

    inquiry database is compeleted

---
+++ demo_gym.js
@@ -0,0 +1,58 @@
+
+// show error in demo field 
+
+const demoform = document.getElementById('form_demo');
+const FirstNameDemo = document.getElementById("first_name_demo");
+const LastNameDemo = document.getElementById("last_name_demo");
+const emailDemo = document.getElementById("email_demo");
+const ageDemo = document.getElementById("age_demo");
+const  date = document.getElementById("date");
+demoform.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+    ValidDemo();
+});
+
+const ValidDemo = () => {
+    const FirstNameValue = FirstNameDemo.value.trim();
+    const LastNameValue = LastNameDemo.value.trim();
+    const emailValue = emailDemo.value.trim();
+    const ageValue = ageDemo.value.trim();
+    const datevalue = date.value.trim();
+
+    const today = new Date();
+    const enteredDate = new Date(datevalue);
+
+
+    if (FirstNameValue === "") {
+        alert("First Name is required");
+    } else if (LastNameValue === '') {
+        alert("Last Name is required");
+    }else if (emailValue === "") {
+        alert("Email is required");
+    } else if (!isValidEmail(emailValue)) {
+        alert("Invalid email address");
+    }else if (ageValue === "") {
+        alert("Age is required");
+    }else if(ageValue<=18 || ageValue >=70){
+        alert("invalid age ");
+    }else if (datevalue === "") {
+        alert(" date is required");
+    }else if (enteredDate < today) {
+        alert("Please enter a future date.");
+    }else if (enteredDate > new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000)) {
+        alert("Date cannot be more than 7 days ahead.");
+    } 
+    else {
+        alert("Submitted");
+        clearFormDemo(); // Clear form after successful submission
+    }
+}
+
+// Clear form fields function
+function clearFormDemo() {
+    FirstNameDemo.value = "";
+    LastNameDemo.value = "";
+    emailDemo.value = "";
+    ageDemo.value = "";
+    date.value = "";
+}
